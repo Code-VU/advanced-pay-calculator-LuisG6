b356e34ee4bcc9abc5f186b9f4dbef5a112be85e
Luis Guevara <luisg@Luiss-MacBook-Pro.local>
2022-02-02 15:28:18 -0600

    updating error handle

--- payCalculator.py
+++ payCalculator.py
@@ -11,7 +11,7 @@ def calculatePay():
         print(f"Pay: {total}")
 
     except:
-        print("Please input a number") 
+        print("Error, please enter numeric input") 
     
     # end assignment
 
